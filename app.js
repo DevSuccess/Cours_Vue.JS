a7b138886d4ba6ebf975b888d4bd0bc837a4962b
ITDevSuccess <raharisontsidiany@gmail.com>
2023-06-06 19:57:38 +0300

    Ajout de methode

--- app.js
+++ app.js
@@ -1,9 +1,20 @@
 const app = Vue.createApp({
     data(){
         return{
-            name: 'Muriel'
+            name: 'Muriel',
+            'autheur': 'TSIDIANY Raharison Muriel',
+            'age': 22,
+            'bio': 'ITDevSuccess'
         }
     },
+    methods: {
+        augmenterAge(){
+            this.age++
+        },
+        diminuerAge(){
+            this.age--
+        }
+    }
 })
 
 app.mount('#app')
